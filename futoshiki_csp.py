d5cb8d5c4c8d2b2fff7787ccd282865779ed06b5
Fengkai Ye <fengkai.ye@mail.utoronto.ca>
2020-02-20 12:36:26 -0500

    futoshiki model 1 done; not tested

--- futoshiki_csp.py
+++ futoshiki_csp.py
@@ -25,10 +25,52 @@ cell of the Futoshiki puzzle.
 from cspbase import *
 import itertools
 
+
 def futoshiki_csp_model_1(futo_grid):
-    ##IMPLEMENT
-    
+    size = len(futo_grid)
+    dom = []
+    for i in range(1, size + 1):
+        dom.append(i)
+    vars = []
+    for i in range(size):
+        row = []
+        for j in range(0, size, 2):
+            row.append(Variable('Q({0}, {1})'.format(i + 1, j // 2 + 1), dom))
+        vars.append(row)
+    cons = []
+    # row inequality constraints
+    for row in range(size):
+        for i in range(size):
+            for j in range(i + 1, size):
+                con = Constraint("C(Q({0}, {1}),Q({0}, {2})".format(row + 1, i + 1, j + 1), [vars[row][i], vars[row][j]])
+                sat_tuples = []
+                greater_sign = False
+                less_sign = False
+                if i + 1 == j:
+                    if futo_grid[row][i * 2 + 1] == ">":
+                        greater_sign = True
+                    elif futo_grid[row][i * 2 + 1] == "<":
+                        less_sign = True
+                for t in itertools.product(dom, dom):
+                    if t[0] != t[1]:
+                        if (not greater_sign or t[0] > t[1]) and (not less_sign or t[0] < t[1]):
+                            sat_tuples.append(t)
+                con.add_satisfying_tuples(sat_tuples)
+                cons.append(con)
+    # column inequality constraints
+    for col in range(size):
+        for i in range(size):
+            for j in range(i + 1, size):
+                con = Constraint("C(Q({1}, {0}),Q({1}, {2})".format(col + 1, i + 1, j + 1),
+                                 [vars[i][col], vars[j][col]])
+                sat_tuples = []
+                for t in itertools.product(dom, dom):
+                    if t[0] != t[1]:
+                        sat_tuples.append(t)
+                con.add_satisfying_tuples(sat_tuples)
+                cons.append(con)
+
 
 def futoshiki_csp_model_2(futo_grid):
     ##IMPLEMENT 
-    
+    pass
